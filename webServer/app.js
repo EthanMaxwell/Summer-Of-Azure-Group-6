e22d33bd2da50be3a9059b83bee18e0a1a9a8155
EthanMaxwell <maxweletha@ecs.vuw.ac.nz>
2022-12-18 04:55:28 +1300

    Added code for accessing blobby boi

--- webServer/app.js
+++ webServer/app.js
@@ -2,9 +2,6 @@ const CosmosClient = require('@azure/cosmos').CosmosClient
 const config = require('./config')
 const TaskList = require('./routes/tasklist')
 const TaskDao = require('./models/taskDao')
-const { BlobServiceClient } = require("@azure/storage-blob");
-const { v1: uuidv1 } = require("uuid");
-require("dotenv").config();
 
 const express = require('express')
 const path = require('path')
@@ -14,6 +11,22 @@ const bodyParser = require('body-parser')
 
 const app = express()
 
+// Blobbies
+const { BlobServiceClient } = require('@azure/storage-blob');
+require('dotenv').config()
+
+const accountName = 'soafeedbackpagestorage';
+const sasToken = 'sp=r&st=2022-12-17T14:50:53Z&se=2023-02-27T22:50:53Z&sv=2021-06-08&sr=c&sig=28n%2Ba912n9apiuXuGiRIW7VtSAtm%2Blz5xP8GcrTwncI%3D';
+if (!accountName) throw Error('Azure Storage accountName not found');
+if (!sasToken) throw Error('Azure Storage accountKey not found');
+
+const blobServiceUri = `https://${accountName}.blob.core.windows.net`;
+
+// https://YOUR-RESOURCE-NAME.blob.core.windows.net?YOUR-SAS-TOKEN
+const blobServiceClient = new BlobServiceClient(
+  `${blobServiceUri}?${sasToken}`,
+  null
+);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'jade')
@@ -47,6 +60,18 @@ taskDao
 
 app.get('/', (req, res, next) => taskList.showTasks(req, res).catch(next))
 app.post('/addFeedback', (req, res, next) => taskList.addTask(req, res).catch(next))
+
+app.get(
+  '/logo', async (req, res) => {
+    let i = 1;
+const iter = blobServiceClient.findBlobsByTags("tagkey='tagvalue'");
+let blobItem = await iter.next();
+while (!blobItem.done) {
+  console.log(`Blob ${i++}: ${blobItem.value.name}`);
+  blobItem = await iter.next();
+}
+  }
+);
 
 app.set('view engine', 'jade')
 
